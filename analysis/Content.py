4021c0613248fe9fd9d46cc3e420e76ecd9767a0
HanYang Ding <cqut11403070337@163.com>
2017-06-21 19:43:26 +0800

    fix bugs

--- analysis/Content.py
+++ analysis/Content.py
@@ -5,9 +5,10 @@
 '''
 from pymongo import MongoClient
 
-
 class content():
     def __init__(self):
         self.client = MongoClient()  # 默认连接 localhost 27017
         self.db = self.client.chatlog
         self.post = self.db.vczh
+
+        # TODO:一个大坑未填
